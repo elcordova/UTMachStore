ec04f516d067357b461ffd73fce293f9b83de5f9
Miguel Angel <angel_loko691@hotmail.com>
2015-01-27 17:19:42 -0500

    eliminar nombre, email de publicidad

--- Proyecto/interfaces/producto.aspx.designer.cs
+++ Proyecto/interfaces/producto.aspx.designer.cs
@@ -13,24 +13,6 @@ namespace Proyecto.interfaces {
     public partial class WebForm1 {
         
         /// <summary>
-        /// Control TextBox1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
-        
-        /// <summary>
-        /// Control TextBox3.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox3;
-        
-        /// <summary>
         /// Control TextBox2.
         /// </summary>
         /// <remarks>
