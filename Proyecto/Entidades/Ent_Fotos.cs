920bf55de020f700181e4978b4fc636473d52cd3
Miguel Angel <angel_loko691@hotmail.com>
2015-01-28 16:58:55 -0500

    \\(^.^)//

---
+++ Proyecto/Entidades/Ent_Fotos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.Entidades
+{
+    public class Ent_Fotos
+    {
+         
+      
+        private int Codigo_Pub;
+        private string Nombre;
+        private string Ruta;
+        
+
+        public Ent_Fotos() 
+        {
+            Codigo_Pub = 0;
+            Nombre = "";
+            Ruta = "";
+           
+        }
+
+        public Ent_Fotos(int codigoPub, string nombre,string ruta)
+        {
+            Codigo_Pub = codigoPub;
+            Nombre = nombre;
+            Ruta = ruta;
+          
+        }
+
+       
+    }
+}
---
+++ Proyecto/LogicaDeNegocio/LNComentario.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.LogicaDeNegocio
+{
+    public class LNComentario
+    {
+    }
+}
