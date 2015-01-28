353a4ce1ec40701fe22361a046cccba90d1f8d0f
luis0794 <luis.tobar.94@hotmail.com>
2015-01-28 00:53:33 -0500

    LogicaDeNegocio_Usuario

---
+++ Proyecto/LogicaDeNegocio/LN_Usuario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.LogicaDeNegocio
+{
+    public class LN_Usuario
+    {
+        dataBase.DatosDataContext DB = new dataBase.DatosDataContext();
+
+        public void insertarUsuario(Entidades.Ent_Usuario usuario) 
+        {
+            DB.insertarUsuario(usuario.Nombre_usu, usuario.Nic_usu, usuario.Direccion_usu,
+                usuario.Passwd_usu, usuario.Email_usu, usuario.Estado_usu, usuario.Cedula_usu);
+            DB.SubmitChanges();
+        }
+
+        public void actualizarUsuario(Entidades.Ent_Usuario usuario, string cedula) 
+        {
+            DB.actualizarUsuario(usuario.Nombre_usu, usuario.Nic_usu, usuario.Direccion_usu,
+                usuario.Passwd_usu, usuario.Email_usu, usuario.Estado_usu, cedula);
+            DB.SubmitChanges();
+        }
+
+        public void eliminarUsuario(string cedula) 
+        {
+            DB.eliminarUsuario(cedula);
+            DB.SubmitChanges();
+        }
+    }
+}
