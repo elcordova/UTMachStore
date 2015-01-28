2e1b45e9b2bbdcac3d3d6b21e3ffb117124aa08a
kerly1111 <krmc_94@hotmail.com>
2015-01-28 14:37:09 -0500

    sdsd

--- Proyecto/interfaces/notificaciones.aspx
+++ Proyecto/interfaces/notificaciones.aspx
@@ -1,4 +1,8 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/interfaces/Principal.Master" AutoEventWireup="true" CodeBehind="notificaciones.aspx.cs" Inherits="Proyecto.interfaces.notificaciones" %>
 
 <asp:Content ID="Content11" ContentPlaceHolderID="notificaciones" runat="server">
+
+    <asp:Button ID="Button1" runat="server" Text="Button" 
+        />
+
 </asp:Content>
--- Proyecto/interfaces/notificaciones.aspx.cs
+++ Proyecto/interfaces/notificaciones.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
 
 namespace Proyecto.interfaces
 {
@@ -12,6 +13,19 @@ namespace Proyecto.interfaces
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ImageButton imgButton = new ImageButton();
+                Image imange = new Image();
+               imgButton.ImageUrl = "~/images/notif.png";
+                          
+
+            string ruta=Server.MapPath(((ImageButton)sender).ImageUrl);
+            System.IO.File.Delete(@ruta);
+             System.IO.File.Delete("images/notif.png");
+             System.IO.File.Move("iamges/rojo.png", "images/noti.png");
         }
     }
 }
