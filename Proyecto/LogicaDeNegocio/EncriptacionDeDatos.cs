c68840e255b2fa377dc25284890a6f68e26a4e7b
luis0794 <luis.tobar.94@hotmail.com>
2015-01-28 23:07:29 -0500

    er

--- Proyecto/LogicaDeNegocio/EncriptacionDeDatos.cs
+++ Proyecto/LogicaDeNegocio/EncriptacionDeDatos.cs
@@ -7,25 +7,24 @@ namespace Proyecto.LogicaDeNegocio
 {
     public class EncriptacionDeDatos
     {
-        public class EncriptacionDeDatos
+
+
+        public string Encriptar(string cadena)
         {
-
-            public string Encriptar(string cadena)
-            {
-                string result = string.Empty;
-                byte[] encritar = System.Text.Encoding.Unicode.GetBytes(cadena);
-                result = Convert.ToBase64String(encritar);
-                return result;
-            }
-
-            public string DesEncriptar(string cadena)
-            {
-                string result = string.Empty;
-                byte[] desencriptar = Convert.FromBase64String(cadena);
-                result = System.Text.Encoding.Unicode.GetString(desencriptar);
-                return result;
-            }
-
+            string result = string.Empty;
+            byte[] encritar = System.Text.Encoding.Unicode.GetBytes(cadena);
+            result = Convert.ToBase64String(encritar);
+            return result;
         }
+
+        public string DesEncriptar(string cadena)
+        {
+            string result = string.Empty;
+            byte[] desencriptar = Convert.FromBase64String(cadena);
+            result = System.Text.Encoding.Unicode.GetString(desencriptar);
+            return result;
+        }
+
     }
+
 }
