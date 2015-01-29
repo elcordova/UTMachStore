ae99a1bca3f162b84e02b248f2c01c31d57851d7
cristhiandres <cris.adc94@gmail.com>
2015-01-29 01:48:25 -0500

    lo

--- Proyecto/interfaces/Principal.Master.designer.cs
+++ Proyecto/interfaces/Principal.Master.designer.cs
@@ -58,13 +58,13 @@ namespace Proyecto.interfaces {
         protected global::System.Web.UI.WebControls.HyperLink HyperLink2;
         
         /// <summary>
-        /// Control nombreUsuario.
+        /// Control lista1.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder nombreUsuario;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder lista1;
         
         /// <summary>
         /// Control contenido_principal.
