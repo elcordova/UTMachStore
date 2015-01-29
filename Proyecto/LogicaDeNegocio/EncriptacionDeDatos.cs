7ce7b32d3980ee0990afa0863ad4e808de40962c
luis0794 <luis.tobar.94@hotmail.com>
2015-01-28 23:04:55 -0500

    muereCordova

--- Proyecto/LogicaDeNegocio/EncriptacionDeDatos.cs
+++ Proyecto/LogicaDeNegocio/EncriptacionDeDatos.cs
@@ -7,22 +7,25 @@ namespace Proyecto.LogicaDeNegocio
 {
     public class EncriptacionDeDatos
     {
-
-        public string Encriptar(string cadena) 
+        public class EncriptacionDeDatos
         {
-            string result = string.Empty;
-            byte[] encritar = System.Text.Encoding.Unicode.GetBytes(cadena);
-            result = Convert.ToBase64String(encritar);
-            return result;
-        }
 
-        public string DesEncriptar(string cadena) 
-        {
-            string result = string.Empty;
-            byte[] desencriptar = Convert.FromBase64String(cadena);
-            result = System.Text.Encoding.Unicode.GetString(desencriptar);
-            return result;
-        }
+            public string Encriptar(string cadena)
+            {
+                string result = string.Empty;
+                byte[] encritar = System.Text.Encoding.Unicode.GetBytes(cadena);
+                result = Convert.ToBase64String(encritar);
+                return result;
+            }
 
+            public string DesEncriptar(string cadena)
+            {
+                string result = string.Empty;
+                byte[] desencriptar = Convert.FromBase64String(cadena);
+                result = System.Text.Encoding.Unicode.GetString(desencriptar);
+                return result;
+            }
+
+        }
     }
 }
