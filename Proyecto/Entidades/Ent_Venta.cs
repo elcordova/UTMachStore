5d88efa7ad4b11118b30a183aa267ca1b9604545
elcordova <erick.lenin.03@gmail.com>
2015-01-29 16:59:54 -0500

    ventas

---
+++ Proyecto/Entidades/Ent_Venta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.Entidades
+{
+    public class Ent_Venta
+    {
+        private string codigo_usu;
+        private string fecha_nego;
+        private string cod_tipo_envio;
+
+
+        public Ent_Venta()
+        {
+
+        }
+    }
+}
