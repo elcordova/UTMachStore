d71fe2a6c191438299f57f283cae1fbc3f1ff969
cristhiandres <cris.adc94@gmail.com>
2015-01-29 04:59:36 -0500

    lololo

--- Proyecto/interfaces/usuario.aspx.cs
+++ Proyecto/interfaces/usuario.aspx.cs
@@ -11,6 +11,7 @@ namespace Proyecto.interfaces
     {
 
         LogicaDeNegocio.LN_Usuario lnUsuario = new LogicaDeNegocio.LN_Usuario();
+        LogicaDeNegocio.EncriptacionDeDatos encrip = new LogicaDeNegocio.EncriptacionDeDatos();
         Entidades.Ent_Usuario usuario = new Entidades.Ent_Usuario();
 
         protected void Page_Load(object sender, EventArgs e)
@@ -26,18 +27,20 @@ namespace Proyecto.interfaces
             TextBoxNombre.Text = datosUsuario.ElementAt(0).nombreComp_Usu;
             TextBoxNick.Text = datosUsuario.ElementAt(0).nic_Usu;
             TextBoxCedula.Text = datosUsuario.ElementAt(0).cedula_Usu;
-            TextBoxContrasena.Text = new LogicaDeNegocio.EncriptacionDeDatos().DesEncriptar(datosUsuario.ElementAt(0).password_Usu);
+            TextBoxContrasena.Text = ""+encrip.DesEncriptar(datosUsuario.ElementAt(0).password_Usu);
             TextBoxDireccion.Text = datosUsuario.ElementAt(0).direccion_Usu;
             TextBoxEmail.Text = datosUsuario.ElementAt(0).email_Usu;
 
             contrasenaTemp = TextBoxContrasena.Text;
+            Console.WriteLine(contrasenaTemp);
+            Console.Read();
          
         }
 
         protected void CheckBoxSi_CheckedChanged(object sender, EventArgs e)
         {
 
-            if (CheckBoxSi.Checked)
+            if (CheckBoxSi.Checked.Equals(true))
             {
                 TextBoxContrasena.Text = "";
                 TextBoxContrasena.Enabled = true;
