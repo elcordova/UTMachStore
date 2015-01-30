ff12f205058f3b7bd4599f47eb9dac3972770600
cristhiandres <cris.adc94@gmail.com>
2015-01-29 21:18:09 -0500

    logeo

--- Proyecto/interfaces/log.aspx.cs
+++ Proyecto/interfaces/log.aspx.cs
@@ -64,21 +64,5 @@ namespace Proyecto.interfaces
                 Response.Write("<script language=javascript>alert('Este NickName no existe');</script>");
             }
         }
-
-         //public static void Show(string message) 
-         //{ 
-         //      // Cleans the message to allow single quotation marks 
-         //      string cleanMessage = message.Replace("'", "\'"); 
-         //      string script = "<script type='text/javascript'>alert('" + cleanMessage + "');</script>"; 
-
-         //      // Gets the executing web page 
-         //      Page page = HttpContext.Current.CurrentHandler as Page; 
-
-         //      // Checks if the handler is a Page and that the script isn't allready on the Page 
-         //      if (page != null && !page.ClientScript.IsClientScriptBlockRegistered("alert")) 
-         //      { 
-         //        page.ClientScript.RegisterClientScriptBlock(typeof(Alert), "alert", script); 
-         //      } 
-         //} 
     }
 }
