38d678c348348a767ea15723c3bb623b7994d474
eme1 <fullok_10@hotmail.com>
2015-02-03 14:44:08 -0500

    TODO BN LISTA DESEOS

--- Proyecto/interfaces/ListaDeseos.aspx.cs
+++ Proyecto/interfaces/ListaDeseos.aspx.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Windows.Forms;
 
 namespace Proyecto.interfaces
 {
@@ -14,7 +15,7 @@ namespace Proyecto.interfaces
         LN_Listadeseos ld = new LN_Listadeseos();
         Ent_Listadeseos eld = new Ent_Listadeseos();
         LN_Usuario lnUsuario = new LN_Usuario();
-
+        static int valor = 0;
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["usuario"] == null)
@@ -26,11 +27,17 @@ namespace Proyecto.interfaces
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            
-            
-            ld.eliminardeseo(2);
+            ld.eliminardeseo(valor);
         }
 
+        protected void GridView_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int index = Convert.ToInt32(e.CommandArgument);
+             valor= Convert.ToInt32(tabladeseo.Rows[index].Cells[0].Text);
+           
+        }
+
+      
 
         public void mostrardeseo()
         {
--- Proyecto/interfaces/producto.aspx.cs
+++ Proyecto/interfaces/producto.aspx.cs
@@ -54,20 +54,20 @@ namespace Proyecto.interfaces
         public void Button1_Click1(object sender, EventArgs e)
         {
             int codigo = lnUsuario.idUsuario(Session["usuario"].ToString());
-            int codpub=  Convert.ToInt16(Session["CodigoPublicacionVista"]);
-            int total=lisdes.contar(codigo, codpub);
-            if (total != 0)
-            {
-                Response.Write("<script language=javascript>alert('EL PRODUCTO YA ESTA EN SU LISTA DE DESEOS');</script>");
-            }
-            else
-            {
+            //int codpub=  Convert.ToInt16(Session["CodigoPublicacionVista"].ToString());
+            //int total=lisdes.contar(codigo, codpub);
+            //if (total != 0)
+            //{
+            //    Response.Write("<script language=javascript>alert('EL PRODUCTO YA ESTA EN SU LISTA DE DESEOS');</script>");
+            //}
+            //else
+            //{
            
                 ld.codigo_Usu = codigo;
                 ld.codigo_Pub = Convert.ToInt16(Session["CodigoPublicacionVista"]);
                 lisdes.insertardeseo(ld);
                 Response.Write("<script language=javascript>alert('EL PRODUCTO A SIDO AÑADIDO A SU LISTA DE DESEOS');</script>");
-            }
+            //}
         }
       
 
