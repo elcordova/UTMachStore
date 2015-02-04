3b8760db23fb32e0f7f7458ebe739c8ca38e42bd
wilyroa <wilyroa@gmail.com>
2015-02-03 22:35:17 -0500

    sasd
    asdasdadasd

---
+++ Proyecto/LogicaDeNegocio/Ln_ListarVentas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.LogicaDeNegocio
+{
+    public class Ln_ListarVentas
+    {
+        dataBase.DatosDataContext DB = new dataBase.DatosDataContext();
+
+        public List<dataBase.listar_VentasResult> ListarVentasTable(int idusuario)
+        {
+            return DB.listar_Ventas(idusuario).ToList();
+        }
+    }
+}
