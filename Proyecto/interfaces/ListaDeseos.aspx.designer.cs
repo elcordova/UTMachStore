0b14450cb83d8f3542c1ff04f90cb48fce21158c
eme1 <fullok_10@hotmail.com>
2015-02-03 20:31:35 -0500

    arreglado deseos

--- Proyecto/interfaces/ListaDeseos.aspx.designer.cs
+++ Proyecto/interfaces/ListaDeseos.aspx.designer.cs
@@ -13,24 +13,6 @@ namespace Proyecto.interfaces {
     public partial class ListaDeseos {
         
         /// <summary>
-        /// Control Button1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
-        
-        /// <summary>
-        /// Control Button2.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button2;
-        
-        /// <summary>
         /// Control PlaceHolder1.
         /// </summary>
         /// <remarks>
